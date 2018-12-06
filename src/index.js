71be93eed3ad281b1e1e78d38cde7073d33682ac
Kevin Ward <antiomiae@gmail.com>
2018-12-05 16:21:31 -0800

    .,

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import sodium from 'libsodium-wrappers'
+import * as tn from 'tweetnacl-ts'
 
 (async (console) => {
-    await sodium.ready
-
     console.time('gen keypair')
-    const keypair = sodium.crypto_sign_keypair()
+    const keypair = tn.sign_keyPair()
     console.timeEnd('gen keypair')
+
+    console.log(keypair)
+
+    console.time('signature')
+    const signature = tn.sign(tn.decodeUTF8('this is the message to sign'), keypair.secretKey)
+    console.timeEnd('signature')
+
+    console.log(tn.encodeHex(signature))
+
+    console.time('signature')
+    const verified = tn.sign_open(signature, keypair.publicKey)
+    console.timeEnd('signature')
+
+    console.log(tn.encodeUTF8(verified))
+
 })(console)
